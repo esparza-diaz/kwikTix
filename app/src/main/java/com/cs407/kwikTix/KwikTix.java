9c20e23df0ee000290a54afbc348bd3578d0af6e
Chucklemunch <charlie.kotula@gmail.com>
2023-10-17 08:11:17 -0500

    created skeleton for login page and main layout for fragments

---
+++ app/src/main/java/com/cs407/kwikTix/KwikTix.java
@@ -0,0 +1,14 @@
+package com.cs407.kwikTix;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KwikTix  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kwiktix_layout);
+    }
+}
